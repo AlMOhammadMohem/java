4c97bc57a2167126c0d79b6e0c4f502f09067c3b
AlMOhammadMohem <almohamadmohab@gmail.com>
2026-07-26 11:57:38 +0300

    Create AppConstants.java

---
+++ src/main/java/constants/AppConstants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class AppConstants {
+
+    private AppConstants() {
+    }
+
+    public static final String PAYER_MANAGEMENT_PATH = "payer-management";
+      public static final String SUCCESS_TOAST_KEYWORD = "success";
+      public static final int TOAST_POLL_MILLIS = 200;
+      public static final int TOAST_TIMEOUT_SECONDS = 6;
+      public static final String DATE_INPUT_FORMAT = "dd/MM/yyyy";
+}
