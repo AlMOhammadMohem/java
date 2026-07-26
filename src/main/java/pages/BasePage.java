56090d7bf464ffca1d6af7d0bb11800766f29b83
AlMOhammadMohem <almohamadmohab@gmail.com>
2026-07-26 12:07:36 +0300

    Create BasePage.java

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+
+    protected final WebDriver driver;
+
+    protected BasePage(WebDriver driver) {
+              this.driver = driver;
+    }
+}
